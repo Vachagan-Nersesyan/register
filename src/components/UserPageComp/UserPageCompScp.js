a3527ce79b69443a3582d5357fbc370c7a2503b2
Vachagan Nersesyan <vach.nersesyan@bk.ru>
2023-09-18 21:04:14 +0400

    adding changes

--- src/components/UserPageComp/UserPageCompScp.js
+++ src/components/UserPageComp/UserPageCompScp.js
@@ -6,11 +6,12 @@ import './UserPageCompStl.css'
 class UserPageComp extends Component {
     render() {
 
-        const { userInfoObj } = this.props
+        const { userInfoObj ,logoutFun } = this.props
 
         return (
             <div className='userinfo_content'>
                 <div className='userinfo_content_container'>
+                    <button onClick={logoutFun}>Logout</button>
                     <div className='userinfo_content_container_in_item'>
                         {userInfoObj.userName}
                     </div>
--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import UserPageComp from './components/UserPageComp/UserPageCompScp'
 class App extends Component {
 
   state = {
-    isAuth: false,
-    userInfo : {}
+    isAuth: localStorage.getItem("user") !== null ? true : false,
+    userInfo : localStorage.getItem("user") !== null ? JSON.parse(localStorage.getItem("user")) : {},
+    isInLclStrg:false
   }
 
 
@@ -18,16 +19,29 @@ class App extends Component {
     
     this.setState({
       isAuth: true,
-      userInfo : {...obj}
+      userInfo : {...obj},
     })
+    
+    localStorage.setItem('user',JSON.stringify({...obj}))
+  }
+
+  logoutFun = () => {
+    localStorage.removeItem('user')
+    this.setState({isAuth  :false})
   }
 
 
   render() {
+
+    debugger
+
+    console.log(localStorage.getItem("user"))
+
     return (
       <div className="App">
+        
 
-        {(!this.state.isAuth) ? <RegisterComp authorisedFunc={this.authorisedFunc} />  : <UserPageComp userInfoObj={this.state.userInfo} />}
+        {(!this.state.isAuth) ? <RegisterComp authorisedFunc={this.authorisedFunc} />  : <UserPageComp logoutFun={this.logoutFun} userInfoObj={this.state.userInfo} />}
 
 
       </div>
